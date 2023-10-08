e67d896fbed68da76da636ebc2eb36cd4d27bed9
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-10-08 08:15:56 +0000

    Page 100% Donegit add .

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const Card = ()=> {
+    return(
+        <div class=" py-4 ">
+        <div class="cardContainer">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+            <div class="col text-center ">
+              <div class="card shadow-sm">
+                <img src="https://picsum.photos/500/325" alt="IMG CARD!" />
+                <div class="card-body"> 
+                <h1>Card title</h1>
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ipsam odit blanditiis, quos aperiam laborum quidem accusantium, sunt eius unde labore, excepturi ab. Est, cum delectus a repudiandae neque quaerat.</p>
+                </div>
+                <div class="d-flex justify-content-center align-items-center border p-3">
+                  <button type="button" class="btn btn-primary">Find Out More!</button>
+                  </div>
+              </div>
+            </div>
+            <div class="col text-center ">
+              <div class="card shadow-sm">
+                <img src="https://picsum.photos/500/325" alt="IMG CARD!" />
+                <div class="card-body"> 
+                <h1>Card title</h1>
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ipsam odit blanditiis, quos aperiam laborum quidem accusantium, sunt eius unde labore, excepturi ab. Est, cum delectus a repudiandae neque quaerat.</p>
+                </div>
+                <div class="d-flex justify-content-center align-items-center border p-3">
+                  <button type="button" class="btn btn-primary">Find Out More!</button>
+                  </div>
+              </div>
+            </div>
+            <div class="col text-center ">
+              <div class="card shadow-sm">
+                <img src="https://picsum.photos/500/325" alt="IMG CARD!" />
+                <div class="card-body"> 
+                <h1>Card title</h1>
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ipsam odit blanditiis, quos aperiam laborum quidem accusantium, sunt eius unde labore, excepturi ab. Est, cum delectus a repudiandae neque quaerat.</p>
+                </div>
+                <div class="d-flex justify-content-center align-items-center border p-3">
+                  <button type="button" class="btn btn-primary">Find Out More!</button>
+                  </div>
+              </div>
+            </div>
+            <div class="col text-center ">
+              <div class="card shadow-sm">
+                <img src="https://picsum.photos/500/325" alt="IMG CARD!" />
+                <div class="card-body"> 
+                <h1>Card title</h1>
+                  <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ipsam odit blanditiis, quos aperiam laborum quidem accusantium, sunt eius unde labore, excepturi ab. Est, cum delectus a repudiandae neque quaerat.</p>
+                </div>
+                <div class="d-flex justify-content-center align-items-center border p-3">
+                  <button type="button" class="btn btn-primary">Find Out More!</button>
+                  </div>
+              </div>
+            </div>
+            
+          </div>
+
+          
+
+        
+
+        </div>
+      </div>
+    );
+
+}
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import NavBar from "./NavBar.jsx";
+import JumboTron from "./JumboTron.jsx";
+import Card from "./Card.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+			<div>
+				<NavBar />		
+				<JumboTron />
+				<Card />
+
+				
+					<div className="footer">
+						<h4 className="text-center py-5 mb-0 text-light">
+							Copyright © Your Website 2023
+						</h4>
+        		</div>
+			</div>
+			
 	);
 };
 
 export default Home;
+
